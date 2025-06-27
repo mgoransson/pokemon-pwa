b12752051d2cbc300ca7a42cf1e6811d42faa92b
mgoransson <marcus.goransson@yahoo.se>
2025-06-27 17:25:30 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,46 @@
+const CACHE_NAME = 'pokemon-go-pwa-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/assets/images/pokemon/pikachu.gif',
+  '/assets/images/pokemon/bulbasaur.gif',
+  '/assets/images/pokemon/charmander.gif',
+  '/assets/images/pokeball.png',
+  '/assets/images/map.jpg',
+  '/assets/images/background.jpg',
+  // lägg till fler assets och pokemon-gifs här
+];
+
+// Install-eventet: cacha viktiga filer
 self.addEventListener('install', event => {
-  console.log('ServiceWorker installerad');
+  console.log('ServiceWorker installerad och cachear assets');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
 });
 
+// Fetch-eventet: svara från cache om möjligt, annars från nätet
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Optional: rensa gammal cache vid aktivering
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
 });
